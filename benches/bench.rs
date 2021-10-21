3e739b24330d909f7f65d9c6d6009cd63a912cf8
dstrub18 <47817771+dstrub18@users.noreply.github.com>
2021-10-21 22:03:53 +0200

    more benchmarks

--- benches/bench.rs
+++ benches/bench.rs
@@ -9,13 +9,50 @@ mod tests
     #[bench]
     fn bench_expensive_corrcoef(b: &mut test::Bencher)
     {
-        let data  = ndarray::Array2::ones((20, 20));
+        let data  = ndarray::Array2::ones((4, 4));
         b.iter(|| simple_statistics::utilities::get_correlation_coefficient_matrix_maybe_expensive(&data));
     }
+
     #[bench]
     fn bench_corrcoef(b: &mut test::Bencher)
     {
-        let data  = ndarray::Array2::ones((20, 20));
+        let data  = ndarray::Array2::ones((4, 4));
         b.iter(|| simple_statistics::utilities::get_correlation_coefficient_matrix(&data));
+    }
+    
+    #[bench]
+    fn bench_test_covariance(b: &mut test::Bencher)
+    {
+        let data  = ndarray::Array2::ones((4, 4));
+        b.iter(|| simple_statistics::utilities::get_covariance_matrix(&data));
+    }
+
+    #[bench]
+    fn bench_mean(b: &mut test::Bencher)
+    {
+        let data  = ndarray::Array1::ones(16);
+        b.iter(|| simple_statistics::utilities::get_mean(&data));
+    }
+
+    #[bench]
+    fn bench_std(b: &mut test::Bencher)
+    {
+        let data  = ndarray::Array1::ones(16);
+        b.iter(|| simple_statistics::utilities::get_standard_deviation(&data));
+    }
+    
+    #[bench]
+    fn bench_variance(b: &mut test::Bencher)
+    {
+        let data  = ndarray::Array1::ones(16);
+        b.iter(|| simple_statistics::utilities::get_variance(&data));
+    }
+    
+    #[bench]
+    fn bench_some_squares(b: &mut test::Bencher)
+    {
+        
+        let data  = ndarray::Array1::ones(16);
+        b.iter(|| simple_statistics::utilities::get_sum_of_squares(&data));
     }
 }
